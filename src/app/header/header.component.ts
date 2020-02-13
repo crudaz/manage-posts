82d8020e92932ec86b001f4f54630ae0923f27cc
crudaz <romerocruz@gmail.com>
2020-02-12 23:28:15 -0500

    feat: header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  handleAction(e) {
+    // console.log('handleAction: ', e);
+  }
+
+}
