cd18fd0b784692eadb96039328feee7a6c4df114
crudaz <romerocruz@gmail.com>
2020-02-13 00:07:40 -0500

    feat: card general component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title = 'manage-posts';
+
+  cardList = [
+    {
+      picture: `https://material.angular.io/assets/img/examples/shiba2.jpg`,
+      text: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+      bred for hunting.`,
+      tags: ['Tag1', 'tag2', 'tag3'],
+      btn: { name: 'View' }
+    }
+  ];
+
 }
--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 
 import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, CardComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
-    ButtonComponent
-  ]
+    ButtonComponent,
+    CardComponent
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class GlobalModule { }
