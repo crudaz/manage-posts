35bb1d9fd1a0355a2c0d502fe8c07092d5a965ff
crudaz <romerocruz@gmail.com>
2020-02-13 22:41:49 -0500

    fix: render card for each item

--- src/app/global/components/card/card.component.ts
+++ src/app/global/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,12 +6,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  @Input() items;
+  @Input() item;
   
   constructor() { }
 
-  ngOnInit(): void {
-    // console.log('card component: ', this.items);
+  ngOnInit() {
+    this.item.photoUrl = `http://localhost:3000/${this.item.photoUrl}`;
   }
-
+  
 }
