e9a8b7504ea7d39e9a4aad86bde4e7fc8500b338
crudaz <romerocruz@gmail.com>
2020-02-13 22:44:00 -0500

    feat: routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'manage-posts';
-
-  cardList = [
-    {
-      picture: `https://material.angular.io/assets/img/examples/shiba2.jpg`,
-      text: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
-      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-      bred for hunting.`,
-      tags: ['Tag1', 'tag2', 'tag3'],
-      btn: { name: 'View' }
-    }
-  ];
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HttpClientModule } from "@angular/common/http";
 import { GlobalModule } from './global/global.module';
 
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    PostsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
     MaterialModule,
     GlobalModule
   ],
